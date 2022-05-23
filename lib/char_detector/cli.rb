8659af63ff2cb3a48d118b67ae0121e8b73ca10f
lijunwei <ljw532344863@sina.com>
2022-05-24 00:06:18 +0800

    Change to namespaced format.

--- lib/char_detector/cli.rb
+++ lib/char_detector/cli.rb
@@ -1,37 +1,39 @@
-class CharDetector::Cli
-  def self.execute(options={})
-    new(options).run
-  end
-
-  attr_reader :options
-
-  def initialize(options={})
-    @options = options
-
-    if options[:file] && !File.exist?(options[:file])
-      warn("[fail] Can't find file: #{options[:file]}")
-      exit 1
+module CharDetector
+  class Cli
+    def self.execute(options={})
+      new(options).run
     end
 
-    if options.empty?
-      abort("[info] Pass -h for usage")
-      exit 1
-    end
-  end
+    attr_reader :options
 
-  def run
-    info = {}
-    info[:file]   = options[:file]
-    info[:result] = CharDetector::Engine.new(file: options[:file]).scan
+    def initialize(options={})
+      @options = options
 
-    unless info[:result].empty?
-      if options[:pretty]
-        jj info
-      else
-        puts info.to_json
+      if options[:file] && !File.exist?(options[:file])
+        warn("[fail] Can't find file: #{options[:file]}")
+        exit 1
+      end
+
+      if options.empty?
+        abort("[info] Pass -h for usage")
+        exit 1
       end
     end
 
-    exit 0
+    def run
+      info = {}
+      info[:file]   = options[:file]
+      info[:result] = CharDetector::Engine.new(file: options[:file]).scan
+
+      unless info[:result].empty?
+        if options[:pretty]
+          jj info
+        else
+          puts info.to_json
+        end
+      end
+
+      exit 0
+    end
   end
 end
--- lib/char_detector/engine.rb
+++ lib/char_detector/engine.rb
@@ -1,34 +1,36 @@
-class CharDetector::Engine
-  def initialize(file:)
-    @file = file
-  end
-
-  attr_reader :file
-
-  def scan
-    matches = []
-
-    File.readlines(file).each_with_index do |line, index|
-      scanned = trim_newline(line).scan(/\p{Cntrl}/)
-      scanned -= trim_newline(line).scan(/\p{Space}/)
-
-      next if scanned.empty?
-
-      hash = {}
-      hash[:line]    = index + 1
-      hash[:columns] = scanned.map { |e| line.index(e)+1 }
-
-      matches << hash
+module CharDetector
+  class Engine
+    def initialize(file:)
+      @file = file
     end
 
-    return matches
-  end
+    attr_reader :file
 
-  def trim_newline(line)
-    if line.end_with?("\n")
-      line[0..-2]
-    else
-      line
+    def scan
+      matches = []
+
+      File.readlines(file).each_with_index do |line, index|
+        scanned = trim_newline(line).scan(/\p{Cntrl}/)
+        scanned -= trim_newline(line).scan(/\p{Space}/)
+
+        next if scanned.empty?
+
+        hash = {}
+        hash[:line]    = index + 1
+        hash[:columns] = scanned.map { |e| line.index(e)+1 }
+
+        matches << hash
+      end
+
+      return matches
+    end
+
+    def trim_newline(line)
+      if line.end_with?("\n")
+        line[0..-2]
+      else
+        line
+      end
     end
   end
 end
